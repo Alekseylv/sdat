c521bd6a9166c5d7785f9aeebf822633ac8e1560
Aleksejs Spuris <aleksejs.spuris@accenture.com>
2017-01-23 11:31:53 +0200

    create empty angular project

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sdatApp
+ * @description
+ * # sdatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sdatApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sdatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sdatApp
+ */
+angular.module('sdatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
