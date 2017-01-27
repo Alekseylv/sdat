042257264cd0a151f43b1a6a63f577c6f93081c7
Alekseylv <alekseylv@me.com>
2017-01-27 15:57:55 +0200

    add dynamic grahps and tables

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,78 @@
  *
  * Main module of the application.
  */
-angular
-  .module('sdatApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
+angular.module('sdatApp', [
+   'ngAnimate',
+   'ngCookies',
+   'ngResource',
+   'ngRoute',
+   'ngSanitize',
+   'ngTouch',
+   'xeditable'
+]).config(function ($routeProvider) {
+   $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+         templateUrl: 'views/main.html',
+         controller: 'MainCtrl',
+         controllerAs: 'main'
       })
       .otherwise({
-        redirectTo: '/'
+         redirectTo: '/'
       });
-  });
+}).directive('visNetwork', function () {
+   return {
+      restrict: 'E',
+      require: '^ngModel',
+      scope: {
+         ngModel: '=',
+         onSelect: '&',
+         options: '='
+      },
+      link: function ($scope, $element) {
+         var network = new vis.Network($element[0], $scope.ngModel, $scope.options || {});
+
+         var onSelect = $scope.onSelect() || function () {};
+         network.on('select', function (properties) {
+            onSelect(properties);
+         });
+      }
+   };
+}).directive('editableTable', function () {
+   return {
+      restrict: 'E',
+      templateUrl: 'views/editable-table.html',
+      require: '^nodes',
+      scope: {
+         nodes: '=',
+         onUpdate: '&',
+         onDelete: '&',
+         onCreate: '&'
+      },
+      controller: function ($scope) {
+         $scope.saveNode = function (data, id) {
+            var node = _.extend(data, {id: id});
+            $scope.onUpdate({node: node});
+            $scope.nodes[_.findIndex($scope.nodes, function (n) {return n.id === id;})] = node;
+         };
+
+         $scope.removeNode = function (index) {
+            var node = $scope.nodes[index];
+            $scope.nodes.splice(index, 1);
+            $scope.onDelete({node: node});
+
+         };
+
+         $scope.addNode = function () {
+            $scope.inserted = {
+               id: $scope.nodes[$scope.nodes.length - 1].id + 1,
+               label: ''
+            };
+            $scope.nodes.push($scope.inserted);
+            $scope.onCreate({node: $scope.inserted});
+         };
+      }
+   };
+});
+
+
+
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,43 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name sdatApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the sdatApp
- */
-angular.module('sdatApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+angular.module('sdatApp').controller('MainCtrl', function ($scope) {
+   this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
-    ];
-  });
+   ];
+
+   $scope.nodes = [
+      {id: 0, label: 'd0'},
+      {id: 1, label: 'd1'},
+      {id: 2, label: 'd2'},
+      {id: 3, label: 'd3'},
+      {id: 4, label: 'd4'}
+   ];
+
+   $scope.edges = _.map($scope.nodes, function () {return [];});
+
+   $scope.ticked = function (from, to) {
+      var edge = {id: from + '-' + to, from: parseInt(from), to: to};
+
+      if ($scope.edges[from][to]) {
+         $scope.networkData.edges.add(edge);
+      } else {
+         $scope.networkData.edges.remove(edge);
+      }
+   };
+
+   $scope.networkData = {
+      nodes: new vis.DataSet($scope.nodes),
+      edges: new vis.DataSet([])
+   };
+
+   $scope.networkOptions = {
+      edges: {
+         arrows: 'to'
+      },
+      height: '500'
+   };
+
+})
+;
