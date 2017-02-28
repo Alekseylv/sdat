6f034ef0372097b5f0e38ac6f0fa62c5fae051fd
Aleksejs Spuris <aleksejs.spuris@accenture.com>
2017-02-28 13:56:13 +0200

    compute reachability and precedent sets

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,6 +26,8 @@ angular.module('sdatApp').controller('MainCtrl', function ($scope) {
         req.nodeDataSet = new vis.DataSet([]);
         req.matrix = [];
         req.reachabilityMatrix = [];
+        req.precedentSet = [];
+        req.reachabilitySet = [];
         req.edges = _.map($scope.nodes, function () {
             return [];
         });
@@ -35,6 +37,14 @@ angular.module('sdatApp').controller('MainCtrl', function ($scope) {
     $scope.requirements = _.map($scope.requirements, $scope.extendRequirement);
 
     $scope.ticked = function (from, to, req) {
+        recomputeEdgesAndNodes(req, from, to);
+        req.matrix = computeMatrix(req);
+        req.reachabilityMatrix = computeReachabilityMatrix(req);
+        req.reachabilitySet = computeReachabilitySet(req);
+        req.precedentSet = computePrecedentSet(req);
+    };
+
+    var recomputeEdgesAndNodes = function (req, from, to) {
         var edge = {id: from + '-' + to, from: parseInt(from), to: to};
 
         if (req.edges[from][to]) {
@@ -46,10 +56,7 @@ angular.module('sdatApp').controller('MainCtrl', function ($scope) {
             removeIfNeeded(req, from);
             removeIfNeeded(req, to);
         }
-        req.matrix = computeMatrix(req);
-        req.reachabilityMatrix = computeReachabilityMatrix(req);
     };
-
 
     var computeMatrix = function (req) {
         var originalMatrix = [];
@@ -70,7 +77,7 @@ angular.module('sdatApp').controller('MainCtrl', function ($scope) {
         var matrix = originalMatrix;
         var iterationCount = 0;
 
-        while (math.max(matrix) > 0) {
+        while (!isEmptyMatrix(matrix) && math.max(matrix) > 0) {
             matrices.push(matrix);
             matrix = math.multiply(matrix, originalMatrix);
             if (iterationCount++ > nodeCount) {
@@ -78,14 +85,41 @@ angular.module('sdatApp').controller('MainCtrl', function ($scope) {
             }
         }
 
+        if (isEmptyMatrix(matrix)) {
+            return [];
+        }
+
         return math.sign(_.reduce(matrices, function (memo, num) {
             return math.add(memo, num);
         }))._data;
     };
 
+
+    var computeReachabilitySet = function (req) {
+        return computeSet(req, req.reachabilityMatrix);
+    };
+
+    var computePrecedentSet = function (req) {
+        return computeSet(req, math.transpose(req.reachabilityMatrix));
+    };
+
+    var computeSet = function (req, matrix) {
+        return _.map(req.nodeDataSet.get(), function (node, index) {
+            return _.chain(matrix[index]).map(function (x, i) {
+                return {reachable: x > 0, label: req.nodeDataSet.get()[i].label};
+            }).filter(function (x) {
+                return x.reachable;
+            }).map(function (x) {
+                return x.label;
+            }).value();
+        });
+    };
+
     // var printMatrix = function (matrix) {
     //     console.log('matrix');
-    //     _.forEach(matrix._data, console.log);
+    //     _.forEach(matrix._data, function (x) {
+    //         console.log(x);
+    //     });
     // };
 
     var removeIfNeeded = function (req, nodeId) {
@@ -104,6 +138,10 @@ angular.module('sdatApp').controller('MainCtrl', function ($scope) {
         return req.edgeDataSet.get().filter(function (edge) {
             return edge.from === nodeId || edge.to === nodeId;
         });
+    };
+
+    var isEmptyMatrix = function (matrix) {
+        return matrix.size(matrix).length <= 1;
     };
 
 });
